bc99b06137605e040a895ecdd01a04fc7f604ae1
Penelope Yong <penelopeysm@gmail.com>
2026-08-18 22:59:54 +0100

    reenable tests (will fail as there are some Enzyme bugs)

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,12 +4,12 @@ using VectorBijectors
 using Test
 
 @testset "VectorBijectors.jl" begin
-    # include("univariate.jl")
-    # include("multivariate.jl")
-    # include("matrix.jl")
-    # include("reshaped.jl")
-    # include("cholesky.jl")
-    # include("order.jl")
+    include("univariate.jl")
+    include("multivariate.jl")
+    include("matrix.jl")
+    include("reshaped.jl")
+    include("cholesky.jl")
+    include("order.jl")
     include("product.jl")
 end
 
