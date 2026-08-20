fe6a01b68f06691bf15b21ee43efa92e0bb15a32
Penelope Yong <penelopeysm@gmail.com>
2026-08-20 15:19:40 +0100

    Reenable Distributions tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,13 +4,13 @@ using Plaice
 using Test
 
 @testset "Plaice.jl" begin
-    # include("distributions/univariate.jl")
-    # include("distributions/multivariate.jl")
-    # include("distributions/matrix.jl")
-    # include("distributions/reshaped.jl")
-    # include("distributions/cholesky.jl")
-    # include("distributions/order.jl")
-    # include("distributions/product.jl")
+    include("distributions/univariate.jl")
+    include("distributions/multivariate.jl")
+    include("distributions/matrix.jl")
+    include("distributions/reshaped.jl")
+    include("distributions/cholesky.jl")
+    include("distributions/order.jl")
+    include("distributions/product.jl")
 
     include("pm/univariate.jl")
 end
