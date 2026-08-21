7902ea2e39b465a574d489311200695824294e4a
Penelope Yong <penelopeysm@gmail.com>
2026-08-22 00:52:48 +0100

    Rename, multivariates

--- ext/PlaiceProbabilityMeasuresExt.jl
+++ ext/PlaiceProbabilityMeasuresExt.jl
@@ -4,10 +4,10 @@ using Plaice
 using ProbabilityMeasures
 using VarNames
 
-const VB = Plaice
 const PM = ProbabilityMeasures
 
 include("ProbabilityMeasures/univariates.jl")
+include("ProbabilityMeasures/multivariates.jl")
 include("ProbabilityMeasures/test_utils.jl")
 
 end # module PlaiceProbabilityMeasuresExt
