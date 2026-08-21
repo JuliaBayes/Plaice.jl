8c8a6d2a83937bf1c12fc2fa7bd9551bd3b0204c
Penelope Yong <penelopeysm@gmail.com>
2026-08-21 19:02:56 +0100

    Remove PM for now

--- ext/ProbabilityMeasures/test_utils.jl
+++
@@ -1,5 +0,0 @@
-using Test: @test
-
-VB.is_continuous(::PM.ContinuousUnivariateMeasure) = true
-VB.test_name(d::PM.AbstractProbabilityMeasure) = repr(d)
-VB.rand_safe_ad(d::PM.AbstractProbabilityMeasure) = rand(d)
