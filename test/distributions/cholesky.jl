8f3a179fb600415f62050bd0dc26c02125a1b857
Ben Cox <bcox@mpp.mpg.de>
2026-09-13 23:45:43 +0200

    Simplify LKJ transforms and Jacobians
    Remove ReverseDiff-specific wrappers and consolidate LKJ kernels under the matrix implementation. Reuse structural factors and log-domain recurrences while preserving coordinates and Jacobians.

--- test/distributions/cholesky.jl
+++ test/distributions/cholesky.jl
@@ -6,15 +6,12 @@ using Test
 using Plaice
 import DifferentiationInterface as DI
 using ForwardDiff: ForwardDiff
-using ReverseDiff: ReverseDiff
 using Mooncake: Mooncake
 using Enzyme: Enzyme, set_runtime_activity, Const, Forward, Reverse
 
 # Need runtime activity for some reason.
 # TODO(penelopeysm): Report upstream
 const adtypes = [
-    DI.AutoReverseDiff(),
-    DI.AutoReverseDiff(; compile=true),
     DI.AutoMooncake(),
     DI.AutoMooncakeForward(),
     DI.AutoEnzyme(; mode=set_runtime_activity(Forward), function_annotation=Const),
@@ -26,8 +23,6 @@ dists = [
     # DifferentiationInterface trips up with empty vectors.
     LKJCholesky(3, 1.0, 'U'),
     LKJCholesky(3, 1.0, 'L'),
-    LKJCholesky(5, 1.0, 'U'),
-    LKJCholesky(5, 1.0, 'L'),
 ]
 
 @testset "Cholesky" begin
--- test/distributions/matrix.jl
+++ test/distributions/matrix.jl
@@ -8,7 +8,6 @@ import DifferentiationInterface as DI
 using Plaice
 using Enzyme: Enzyme
 using ForwardDiff: ForwardDiff
-using ReverseDiff: ReverseDiff
 using Mooncake: Mooncake
 
 ν = 5
@@ -16,10 +15,12 @@ M = [1 2 3; 4 5 6]
 Σ = PDMats.PDMat([1 0.5; 0.5 1])
 Ω = PDMats.PDMat([1 0.3 0.2; 0.3 1 0.4; 0.2 0.4 1])
 
-# TODO(penelopeysm): ReverseDiff gives wrong results when differentiating
-# through VecCorrBijector. Correctness tests are disabled for now.
-# https://github.com/TuringLang/Bijectors.jl/issues/434
-lkj_test_adtypes = [DI.AutoMooncake(), DI.AutoMooncakeForward()]
+const adtypes = [
+    DI.AutoMooncake(),
+    DI.AutoMooncakeForward(),
+    DI.AutoEnzyme(; mode=Enzyme.Forward, function_annotation=Enzyme.Const),
+    DI.AutoEnzyme(; mode=Enzyme.Reverse, function_annotation=Enzyme.Const),
+]
 
 # Don't check that from_unconstrained_vec(d)(randn(...)) is in support for LKJ,
 # The reason is because the inverse bijector for LKJ causes the diagonal
@@ -30,27 +31,24 @@ test_in_support(d) = !(d isa LKJ)
 
 matrix_dists = [
     MatrixNormal(2, 4),
-    MatrixNormal(3, 5),
     MatrixTDist(ν, M, Σ, Ω),
-    Wishart(7, Matrix{Float64}(I, 2, 2)),
     Wishart(7, Matrix{Float64}(I, 4, 4)),
     InverseWishart(7, Matrix{Float64}(I, 2, 2)),
-    InverseWishart(7, Matrix{Float64}(I, 4, 4)),
-    # See note in matrix/posdef.jl
-    # MatrixBeta(3, 3, 1000),
-    # MatrixBeta(5, 8, 1000),
     LKJ(3, 1.0),
-    LKJ(7, 1.0),
 ]
 
 @testset "Matrix distributions" begin
+    @testset "Correlation inverse Jacobian at large coordinates" begin
+        f = from_unconstrained_vec(LKJ(3, 1.0))
+        y = [800.0, 0.0, 0.0]
+        logjac = 3 * (log(2) - 800)
+        @test logabsdet_jacobian(f, y) ≈ logjac
+        @test last(with_logabsdet_jacobian(f, y)) ≈ logjac
+    end
+
     for d in matrix_dists
         kwargs = (expected_zero_allocs=(), test_in_support=test_in_support(d))
-        if d isa LKJ
-            Plaice.test_all(d; adtypes=lkj_test_adtypes, kwargs...)
-        else
-            Plaice.test_all(d; kwargs...)
-        end
+        Plaice.test_all(d; adtypes=adtypes, kwargs...)
     end
 end
 
